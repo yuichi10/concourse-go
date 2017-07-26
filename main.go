69710572e3dd8bc17a23dc202be658f3eaac1192
yuichi10 <y_084743@yahoo.co.jp>
2017-07-26 19:04:32 +0900

    create easy server

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/yuichi10/concourse-go/util"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello")
+	fmt.Fprintln(w, "5 + 3=", util.Add(5, 3))
+	fmt.Fprintln(w, "5 - 3=", util.Sub(5, 3))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) <= 0 {
+		port = ":8888"
+	}
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(port, nil)
+}
